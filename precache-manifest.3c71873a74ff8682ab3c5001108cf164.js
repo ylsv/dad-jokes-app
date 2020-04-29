e70a87f3cf303c87e62dfb3e6510e6fefe583b68
ylsv <lisov1988@gmail.com>
2020-04-29 13:18:01 +0300

    Updates

---
+++ precache-manifest.3c71873a74ff8682ab3c5001108cf164.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64fb0cc5d78fb2caccbecf1a3df6756f",
+    "url": "/dad-jokes-app/index.html"
+  },
+  {
+    "revision": "a174a6ba78533bd42a7b",
+    "url": "/dad-jokes-app/static/css/main.01eae7d6.chunk.css"
+  },
+  {
+    "revision": "77aaacd1c4016c1eb433",
+    "url": "/dad-jokes-app/static/js/2.24733761.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/dad-jokes-app/static/js/2.24733761.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a174a6ba78533bd42a7b",
+    "url": "/dad-jokes-app/static/js/main.696bc6ab.chunk.js"
+  },
+  {
+    "revision": "e68e6ec9d79bd5ea2b62",
+    "url": "/dad-jokes-app/static/js/runtime-main.d1b17c41.js"
+  }
+]);
